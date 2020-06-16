30ec9dcc22c544f1a7261a49240a144efd1ace79
Shotaro Hirose <shotaro199959@icloud.com>
2020-06-16 22:20:43 +0900

    [Fix] サーヴァント一覧ページを本格修正

--- pages/characters/index.vue
+++ pages/characters/index.vue
@@ -1,26 +1,41 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">No.</th>
-            <th class="text-left">キャラクター名</th>
-            <th class="text-left">クラス</th>
-            <th class="text-left">レア</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="character in characters" :key="character.id">
-            <td>{{ character.number }}</td>
-            <td>{{ character.name }}</td>
-            <td>{{ character.class }}</td>
-            <td>{{ character.rarity }}</td>
-          </tr>
-        </tbody>
-      </template>
-    </v-simple-table>
-  </v-layout>
+  <v-row no-gutters>
+    <v-card class="col-md-12">
+      <v-toolbar class="title" elevation="8">
+        サーヴァント一覧
+        <v-row no-gutters>
+          <v-col style="text-align: right;">
+            <v-btn
+              outlined
+              small
+              fab
+              class="mr-3"
+              color="purple lighten-1"
+              @click="openDisplay()"
+            >
+              <v-icon>mdi-help</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-toolbar>
+      <v-card-subtitle>
+        単体宝具、全体宝具を持つサーヴァントのみ
+      </v-card-subtitle>
+      <v-card-text v-for="character in characters" :key="character.id">
+        <v-row no-gutters>
+          <div>
+            <div class="subtitle-1">{{ character.name }}</div>
+            <span class="caption">
+              ★{{ character.rarity }} / {{ character.class }} /
+              {{ character.card }}宝具
+            </span>
+          </div>
+          <v-spacer />
+          No. {{ character.number }}
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-row>
 </template>
 
 <script>
@@ -38,9 +53,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-th {
-  width: 200px;
-}
-</style>
