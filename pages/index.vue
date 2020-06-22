faeb4778d1e1e7da7cf15e413539548763d16329
Shotaro Hirose <shotaro199959@icloud.com>
2020-06-23 03:30:03 +0900

    [Add] スマホの場合 v-bottom-navigationを表示

--- pages/index.vue
+++ pages/index.vue
@@ -87,6 +87,24 @@
         </v-card>
       </v-hover>
     </v-col>
+    <v-bottom-navigation
+      v-if="$vuetify.breakpoint.xs"
+      app
+      fixed
+      color="purple lighten-1"
+    >
+      <!-- ボトムナビゲーション -->
+      <v-btn
+        v-for="menu in menus"
+        :key="menu.title"
+        :to="menu.to"
+        width="100%"
+        color="grey darken-4"
+      >
+        <span>{{ menu.title }}</span>
+        <v-icon>{{ menu.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-row>
 </template>
 
@@ -96,7 +114,25 @@ export default {
   data() {
     return {
       image_src: require('assets/jeannearuta.png'),
-      image_src2: require('assets/altria.png')
+      image_src2: require('assets/altria.png'),
+      menus: [
+        { title: 'ホーム', icon: 'mdi-home', to: '/' },
+        {
+          title: '宝具ダメ計算',
+          icon: 'mdi-sword-cross',
+          to: '/npatk-calculation'
+        },
+        {
+          title: '宝具NP計算',
+          icon: 'mdi-sword',
+          to: '/npaquisition-calculation'
+        },
+        {
+          title: '霊基一覧',
+          icon: 'mdi-file-document-outline',
+          to: '/characters'
+        }
+      ]
     }
   }
 }
