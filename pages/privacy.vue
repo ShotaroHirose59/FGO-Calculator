2919286d52cae07df220757cc23dba35c6ff4e0d
Shotaro Hirose <shotaro199959@icloud.com>
2020-06-25 19:22:31 +0900

    プライバシーポリシー作成

---
+++ pages/privacy.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="privacy">
+    <h1>プライバシーポリシー</h1>
+    <p>
+      当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。
+    </p>
+    <p>
+      このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。
+      この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。
+    </p>
+    <p>
+      この規約に関しての詳細は<a
+        href="https://marketingplatform.google.com/about/analytics/terms/jp/"
+        rel="noopener noreferrer"
+        target="_blank"
+        class="external ext_icon"
+        >Googleアナリティクスサービス利用規約</a
+      >
+      のページや<a
+        href="https://policies.google.com/technologies/ads?hl=ja"
+        rel="noopener noreferrer"
+        target="_blank"
+        class="external ext_icon"
+        >Googleポリシーと規約</a
+      >
+      ページをご覧ください。
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+.v-application p {
+  line-height: 1.7rem;
+  margin-top: 0.3rem;
+  margin-left: 10px;
+  margin-bottom: 0.3rem;
+}
+.privacy {
+  margin-bottom: 20px;
+}
+</style>
