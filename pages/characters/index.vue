d38793faa991cff335e4a2e18ee3503072abe9f5
Shotaro Hirose <shotaro199959@icloud.com>
2020-06-30 19:35:23 +0900

    [Delete] いらないコード削除

--- pages/characters/index.vue
+++ pages/characters/index.vue
@@ -34,26 +34,7 @@ export default {
     BottomNavigation
   },
   data() {
-    return {
-      menus: [
-        { title: 'ホーム', icon: 'mdi-home', to: '/' },
-        {
-          title: '宝具ダメ計算',
-          icon: 'mdi-sword-cross',
-          to: '/npatk-calculation'
-        },
-        {
-          title: '宝具NP計算',
-          icon: 'mdi-gauge-full',
-          to: '/npaquisition-calculation'
-        },
-        {
-          title: '霊基一覧',
-          icon: 'mdi-file-document-outline',
-          to: '/characters'
-        }
-      ]
-    }
+    return {}
   },
   computed: {
     characters() {
