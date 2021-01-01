712b2ddfc78b43a7d2dd6b350242df45f775e210
Shotaro Hirose <shotaro199959@icloud.com>
2021-01-02 02:31:26 +0900

    lint修正

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
-const functions = require('firebase-functions');
-const { Nuxt } = require('nuxt');
+const functions = require('firebase-functions')
+const { Nuxt } = require('nuxt')
 const nuxt = new Nuxt({
   buildDir: 'ssr',
   dev: false
-});
+})
 
 exports.ssr = functions.https.onRequest(async (req, res) => {
-    await nuxt.ready()
-    return nuxt.render(req, res)
+  await nuxt.ready()
+  return nuxt.render(req, res)
 })
