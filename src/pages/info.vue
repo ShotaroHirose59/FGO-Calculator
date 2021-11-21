04b9012fafe9fd4511a27e9a396ef93f4a5db65f
ShotaroHirose59 <shotaro199959@icloud.com>
2021-11-21 23:47:17 +0900

    Delete: ページネーションを撤去した

--- src/pages/info.vue
+++ src/pages/info.vue
@@ -17,49 +17,22 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="info in infoLists" :key="info.id">
+              <tr v-for="info in information" :key="info.id">
                 <td>{{ info.created }}</td>
                 <td>{{ info.content }}</td>
               </tr>
             </tbody>
           </template>
         </v-simple-table>
-        <div class="text-center">
-          <v-pagination
-            v-model="page"
-            :length="length"
-            color="purple lighten-1"
-            class="mt-4"
-            @input="pageChange"
-          ></v-pagination>
-        </div>
       </div>
     </v-card>
   </v-row>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      page: 1,
-      pageSize: 10
-    }
-  },
   computed: {
     information() {
       return this.$store.state.info.info
-    },
-    infoLists: {
-      get() {
-        return this.information.slice(
-          this.pageSize * (this.page - 1),
-          this.pageSize * this.page
-        )
-      },
-      set(value) {}
-    },
-    length() {
-      return Math.ceil(this.information.length / this.pageSize)
     }
   },
   created() {
@@ -80,9 +53,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.v-pagination {
-  margin-bottom: 16px;
-}
-</style>
