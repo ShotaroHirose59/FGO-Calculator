c78ce7556b0dc3154d661030b977f966b3cf4817
Shotaro Hirose <shotaro199959@icloud.com>
2022-09-15 23:55:06 +0900

    Fix: ぐだぐだ新邪馬台国の特攻バフ

--- src/mixins/event-buff.js
+++ src/mixins/event-buff.js
@@ -3,82 +3,39 @@ export default {
     setEventCharacterBuff(character) {
       this.isEventCharacter = false
       this.isNpBuffEventCharacter = false
-      // 水着イベント2022
+      // ぐだぐだ新邪馬台国
       if (
-        character.name === 'オルトリンデ' ||
-        character.name === 'スルーズ' ||
-        character.name === 'ヒルド' ||
-        character.name === '水着伊吹童子' ||
-        character.name === '水着スカディ' ||
-        character.name === 'レディ･アヴァロン'
+        character.name === '千利休' ||
+        character.name === '山南敬助' ||
+        character.name === '壱与'
       ) {
         this.isEventCharacter = true
         this.sAtkBuff = 100
       } else if (
-        character.name === '水着ガレス' ||
-        character.name === '燕青' ||
-        character.name === '蘆屋道満' ||
-        character.name === '水着宇津見エリセ' ||
-        character.name === '水着武則天' ||
-        character.name === 'アルクェイド'
+        character.name === '沖田総司' ||
+        character.name === '斎藤一' ||
+        character.name === '茶々' ||
+        character.name === '森長可' ||
+        character.name === '魔王信長' ||
+        character.name === '沖田総司〔オルタ〕'
       ) {
         this.isEventCharacter = true
         this.sAtkBuff = 50
       } else if (
         character.name === '水着沖田総司〔オルタ〕' ||
-        character.name === 'シグルド' ||
-        character.name === '水着葛飾北斎' ||
-        character.name === 'ジークフリート' ||
-        character.name === '水着女王メイヴ' ||
-        character.name === '水着巴御前' ||
-        character.name === '水着フラン' ||
-        character.name === '水着アルトリア' ||
-        character.name === '水着ジャンヌ' ||
-        character.name === 'アナスタシア＆ヴィイ' ||
-        character.name === '水着アン&メアリー' ||
-        character.name === '水着イリヤスフィール' ||
-        character.name === 'エミヤ' ||
-        character.name === '水着エレナ' ||
-        character.name === '水着刑部姫' ||
-        character.name === 'ロビンフッド' ||
-        character.name === '水着玉藻の前' ||
-        character.name === '水着茨木童子' ||
-        character.name === '水着清姫' ||
-        character.name === '水着虞美人' ||
-        character.name === '謎のアルターエゴ・Λ' ||
-        character.name === '水着源頼光' ||
-        character.name === 'アキレウス' ||
-        character.name === '水着アルトリア〔オルタ〕' ||
-        character.name === '水着イシュタル' ||
-        character.name === '水着カーミラ' ||
-        character.name === '水着カイニス' ||
-        character.name === '水着紫式部' ||
-        character.name === '水着モードレッド' ||
-        character.name === 'マンドリカルド' ||
-        character.name === 'ティーチ' ||
-        character.name === '水着ネロ' ||
-        character.name === 'ギルガメッシュ（キャスター）' ||
-        character.name === '水着シャルロット・コルデー' ||
-        character.name === '水着マリー' ||
-        character.name === '水着牛若丸' ||
-        character.name === 'オキタ･J･ソウジ' ||
-        character.name === '水着スカサハ' ||
-        character.name === '水着ニトクリス' ||
-        character.name === '風魔小太郎' ||
-        character.name === '水着宮本武蔵' ||
-        character.name === '水着織田信長' ||
-        character.name === '水着ジャンヌ〔オルタ〕' ||
-        character.name === '水着清少納言' ||
-        character.name === '水着ブリュンヒルデ' ||
-        character.name === '水着アルトリア（ルーラー）' ||
-        character.name === '水着マルタ' ||
-        character.name === '水着ダヴィンチ' ||
-        character.name === '水着カーマ' ||
-        character.name === '巌窟王' ||
-        character.name === '水着殺生院キアラ' ||
-        character.name === '水着BB' ||
-        character.name === 'アビゲイル〔夏〕' ||
-        character.name === '謎のヒロインXX'
+        character.name === '織田信長' ||
+        character.name === '坂本龍馬（ライダー）' ||
+        character.name === '長尾景虎' ||
+        character.name === '坂本龍馬（ランサー）' ||
+        character.name === 'メドゥーサ' ||
+        character.name === '出雲阿国' ||
+        character.name === '謎のヒロインX' ||
+        character.name === '李書文（アサシン）' ||
+        character.name === '水着沖田総司' ||
+        character.name === '岡田以蔵' ||
+        character.name === '土方歳三' ||
+        character.name === '謎の蘭丸X' ||
+        character.name === '水着織田信長'
       ) {
         this.isEventCharacter = true
         this.sAtkBuff = 30
