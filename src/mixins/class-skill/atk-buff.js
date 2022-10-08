49fe51605aa256008c581cff9222699035899144
Shotaro Hirose <shotaro199959@icloud.com>
2022-10-08 17:48:02 +0900

    fix

--- src/mixins/class-skill/atk-buff.js
+++ src/mixins/class-skill/atk-buff.js
@@ -2,10 +2,10 @@ export default {
   methods: {
     setClassSkillAtkBuff(character) {
       if (character.name === 'カレン') {
-        this.atkBuff = 20
+        this.atkBuff += 20
         this.classSkills.push({
           name: '[被虐霊媒体質]',
-          description: '弱体状態時に攻撃力UP(20%)'
+          description: '弱体状態時に攻撃力アップ(20%)'
         })
       }
     }
