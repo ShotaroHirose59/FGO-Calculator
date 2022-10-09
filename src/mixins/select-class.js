3ca807ada99984ba34495a982caa549189397e50
Shotaro Hirose <shotaro199959@icloud.com>
2022-10-09 17:40:23 +0900

    Fix: remove placeholder

--- src/mixins/select-class.js
+++ src/mixins/select-class.js
@@ -6,12 +6,6 @@ export default {
       )
       return characters.map((character) => character.name)
     },
-    $_placeholder() {
-      if (this.characterClass === '') return '先にクラスを選択'
-      if (this.$_is_empty) return '対象がいません'
-
-      return 'サーヴァントを選択'
-    },
     $_is_empty() {
       if (this.$_filteredCharacters.length === 0) return true
     }
