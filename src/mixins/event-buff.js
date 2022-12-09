1c15f6b3f592c67eaa64eb126fd1ca845c2cdd1d
Shotaro Hirose <shotaro199959@icloud.com>
2022-12-10 02:35:58 +0900

    fix

--- src/mixins/event-buff.js
+++ src/mixins/event-buff.js
@@ -10,7 +10,7 @@ export default {
         this.sAtkBuff += 100
       } else if (
         character.name === 'アルジュナ' ||
-        character.name === 'アルテラ･ザ･サン〔タ〕' ||
+        character.name === 'アルテラ・ザ・サン〔タ〕' ||
         character.name === 'ナイチンゲール〔サンタ〕' ||
         character.name === 'カルナ' ||
         character.name === 'ジャンヌサンタリリィ' ||
@@ -19,7 +19,7 @@ export default {
         character.name === 'ゲオルギウス' ||
         character.name === 'アルジュナ〔オルタ〕' ||
         character.name === 'ベオウルフ' ||
-        character.name === 'ケツァル･コアトル〔サンバ／サンタ〕' ||
+        character.name === 'ケツァルコアトル〔サンバ／サンタ〕' ||
         character.name === '水着マルタ'
       ) {
         this.isEventCharacter = true
@@ -31,7 +31,6 @@ export default {
         character.name === 'アシュヴァッターマン' ||
         character.name === 'エリザベート' ||
         character.name === '謎のアルターエゴ・Λ' ||
-        character.name === 'エリザベート〔シンデレラ〕' ||
         character.name === 'マルタ' ||
         character.name === '赤兎馬' ||
         character.name === 'ジーク' ||
