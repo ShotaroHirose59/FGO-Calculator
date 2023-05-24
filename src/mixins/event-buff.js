d962e65cdffe9270d2e463b0bcb636fdb7a0a5ad
Shotaro Hirose <shotaro199959@icloud.com>
2023-05-24 23:12:07 +0900

    風雲からくりイリヤ城のイベ特攻バフ

--- src/mixins/event-buff.js
+++ src/mixins/event-buff.js
@@ -1,12 +1,14 @@
 export default {
   methods: {
     setEventCharacterBuff(characterName) {
-      // 巡霊の祝祭 第2弾
+      // 風雲からくりイリヤ城
       const eventCharacters = {
-        'エリザベート〔ブレイブ〕': 50,
-        織田信長: 50,
-        'エリザベート〔ハロウィン〕': 50,
-        '謎のアイドルX〔オルタ〕': 50
+        果心居士: 100,
+        クロエ: 50,
+        イリヤ: 50,
+        加藤段蔵: 50,
+        風魔小太郎: 50,
+        シトナイ: 50
       }
       if (
         Object.prototype.hasOwnProperty.call(eventCharacters, characterName)
