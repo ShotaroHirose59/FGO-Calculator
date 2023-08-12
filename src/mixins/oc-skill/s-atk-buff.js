48b2fcbe59e36797ecd91956ee06c760824832d7
Shotaro Hirose <shotaro199959@icloud.com>
2023-08-12 12:59:38 +0900

    水着アルキャスにOCの特攻バフの値が変更されても必ず+100するようにした(暫定対応)

--- src/mixins/oc-skill/s-atk-buff.js
+++ src/mixins/oc-skill/s-atk-buff.js
@@ -13,25 +13,32 @@ export default {
       } else if (characterName === '俵藤太') {
         if (this.isActiveSpecialAtkBuff === true) {
           const ocBuffs = [50, 62.5, 75, 87.5, 100]
-          this.sAtkBuff += ocBuffs[this.selectingOcUpPrcentage - 1]
+          this.sAtkBuff = ocBuffs[this.selectingOcUpPrcentage - 1]
         }
         this.setOcSkills('(魔性)特攻を付与(50~100%)')
+      } else if (characterName === '水着アルトリア・キャスター') {
+        if (this.isActiveSpecialAtkBuff === true) {
+          const ocBuffs = [50, 62.5, 75, 87.5, 100]
+          this.sAtkBuff = ocBuffs[this.selectingOcUpPrcentage - 1]
+          this.sAtkBuff += 100 // +100%固定 (サバフェス2023)
+        }
+        this.setOcSkills('(混沌・神性)特攻を付与(50~100%)')
       } else if (characterName === 'ジャック') {
         if (this.isActiveSpecialAtkBuff === true) {
           const ocBuffs = [50, 62.5, 75, 87.5, 100]
-          this.sAtkBuff += ocBuffs[this.selectingOcUpPrcentage - 1]
+          this.sAtkBuff = ocBuffs[this.selectingOcUpPrcentage - 1]
         }
         this.setOcSkills('(女性)特攻を付与(50~100%)')
       } else if (characterName === '紅閻魔') {
         if (this.isActiveSpecialAtkBuff === true) {
           const ocBuffs = [20, 25, 30, 35, 40]
-          this.sAtkBuff += ocBuffs[this.selectingOcUpPrcentage - 1]
+          this.sAtkBuff = ocBuffs[this.selectingOcUpPrcentage - 1]
         }
         this.setOcSkills('(混沌・悪)特攻を付与(40~60%)')
       } else if (characterName === '渡辺綱') {
         if (this.isActiveSpecialAtkBuff === true) {
           const ocBuffs = [20, 25, 30, 35, 40]
-          this.sAtkBuff += ocBuffs[this.selectingOcUpPrcentage - 1]
+          this.sAtkBuff = ocBuffs[this.selectingOcUpPrcentage - 1]
         }
         this.setOcSkills('(魔性)特攻を付与(20~40%)')
       }
